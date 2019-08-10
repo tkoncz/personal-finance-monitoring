3bba2de477ad90f8e60b21e9665e8f19d6fe380f
tkoncz <tamas.koncz@emarsys.com>
2019-08-10 10:59:02 +0200

    :wrench: revert to tokenized auth.

--- global.R
+++ global.R
@@ -1,4 +1,5 @@
 source('libraries.R')
 invisible(sapply(list.files("R", full.names = TRUE), source))
 
+# googlesheets::gs_auth(new_user = TRUE)
 googlesheets::gs_auth(token = Sys.getenv("GS_TOKEN"))
