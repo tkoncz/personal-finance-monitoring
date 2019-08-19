083047bbbd026030cdd4a6172d8e55913dad7680
tkoncz <tamas.koncz@emarsys.com>
2019-08-19 15:20:39 +0200

    :white_check_mark: add new unit tests
    - data & timestamp formatting
    - Subcategory
    - Other Currency

--- tests/testthat/test-raw_google_sheet_parsing_tools.R
+++ tests/testthat/test-raw_google_sheet_parsing_tools.R
@@ -1,3 +1,59 @@
+context("Fixing field formats")
+
+test_that("Date & Timestamp columns are converted to correct format", {
+    test_table <- data.table(
+        Timestamp = "7/29/2019 13:07:10",
+        Date = "7/29/2019"
+    )
+
+    fixDateFormat_(test_table)
+
+    expect_equal(
+        lapply(test_table, class) %>% names(),
+        c("Timestamp", "Date")
+    )
+
+    expect_equal(
+        test_table,
+        data.table(
+            Timestamp = as.POSIXct("2019-07-29 13:07:10", tz = "CET"),
+            Date      = as.Date("2019-07-29")
+        )
+    )
+})
+
+test_that("Subcategory column is created with the correct contents", {
+    test_table <- data.table(
+        Category = c("foo",        "foo",          "bar"),
+        foo      = c("baz",        "baz",          NA_character_),
+        bar      = c(NA_character_, NA_character_, "foobar")
+    )
+
+    addSubCategory_(test_table)
+
+    expect_true(!is.null(test_table[["Subcategory"]]))
+
+    expect_equal(
+        test_table[, Subcategory],
+        c("baz", "baz", "foobar")
+    )
+
+})
+
+test_that("Currency is correctly updated for 'Other'", {
+    test_table <- data.table(
+        Currency                  = c("HUF",         "HUF",         "EUR",         "Other"),
+        `Specify other currency:` = c(NA_character_, NA_character_, NA_character_, "JPY")
+    )
+
+    fixOtherCurrency_(test_table)
+
+    expect_equal(
+        test_table[, Currency],
+        c("HUF", "HUF", "EUR", "JPY")
+    )
+})
+
 context("Adjust records corresponding multiple people")
 
 test_that("Simple case when everyone pays for themselves works", {
