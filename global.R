ab0df357a1753acba4538f63c7fe1badb72b64de
tkoncz <tamas.koncz@emarsys.com>
2020-04-26 13:04:52 +0200

    :recycle: refactor libraries.R -> packages.txt

--- global.R
+++ global.R
@@ -1,2 +1,6 @@
-source('libraries.R')
+invisible(suppressPackageStartupMessages(lapply(
+    trimws(readLines("packages.txt")),
+    function(x) library(x, character.only = TRUE)
+)))
+
 invisible(sapply(list.files("R", full.names = TRUE), source))
